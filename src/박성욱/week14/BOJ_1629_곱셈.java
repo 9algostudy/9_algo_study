d52bbf1d46f0f4b25a993edf85f169cd2a978aca
respectwo2 <respectwo@gmail.com>
2024-12-26 14:21:53 +0900

    test: commit test

--- src/박성욱/week14/BOJ_1629_곱셈.java
+++ src/박성욱/week14/BOJ_1629_곱셈.java
@@ -3,7 +3,8 @@ package 박성욱.week14;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-//주석테스트123123
+
+//커밋테스트입니다
 public class BOJ_1629_곱셈 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));

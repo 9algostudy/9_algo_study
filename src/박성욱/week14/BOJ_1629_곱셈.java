91229460fc186192c31f130cf1c7bd0e760cf3ff
respectwo2 <respectwo@gmail.com>
2024-12-26 12:44:49 +0900

    solv: 14주차 과제 upload

--- src/박성욱/week14/BOJ_1629_곱셈.java
+++ src/박성욱/week14/BOJ_1629_곱셈.java
@@ -3,7 +3,7 @@ package 박성욱.week14;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+//주석테스트
 public class BOJ_1629_곱셈 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
